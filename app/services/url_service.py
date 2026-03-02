3441870e556705cacf9a03918c61bca6037ccf1a
syke-x <sujal.work.26@gmail.com>
2026-03-02 11:37:17 +0530

    added logging and exception handling in url services

--- app/services/url_service.py
+++ app/services/url_service.py
@@ -61,20 +61,35 @@ def create_short_url(long_url, ttl_seconds):
 
 def create_custom_short_url(long_url , custom_short_code , ttl_seconds):
 
-    if not is_valid_url(custom_short_code):
-        return error_response("Invalid custom short code. It should be 3-20 characters long and can only contain letters, numbers, underscores, or hyphens.", 400)
+    logger.info(f"Attempting to create short URL for long URL")
 
-    if URL.query.filter_by(short_code=custom_short_code).first():
-        return error_response("Custom short code already exists", 400)
+    try:
+
+        if not is_valid_url(custom_short_code):
+            return error_response("Invalid custom short code. It should be 3-20 characters long and can only contain letters, numbers, underscores, or hyphens.", 400)
+
+        if URL.query.filter_by(short_code=custom_short_code).first():
+            return error_response("Custom short code already exists", 400)
+        
+        new_url = URL(
+            long_url = long_url,
+            short_code = custom_short_code,
+            count= 0,
+            expired_at = datetime.utcnow() + timedelta(seconds=ttl_seconds)
+        )
+
+        db.session.add(new_url)
+        db.session.commit()
+
+        return success_response(data={"short_code": custom_short_code}, message="Custom short URL created successfully", status_code=201)
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f"Database error while creating custom short URL: {e}")
+        return error_response("Database error occurred", 500)
+    except Exception as e:
+        db.session.rollback()
+        logger.critical(f"Unexpected error while creating short URL: " , exc_info=True)
+        return error_response("An unexpected server error occurred", 500)
+
     
-    new_url = URL(
-        long_url = long_url,
-        short_code = custom_short_code,
-        count= 0,
-        expired_at = datetime.utcnow() + timedelta(seconds=ttl_seconds)
-    )
-
-    db.session.add(new_url)
-    db.session.commit()
-
-    return success_response(data={"short_code": custom_short_code}, message="Custom short URL created successfully", status_code=201)
